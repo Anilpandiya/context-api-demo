bcb80a4f8ae6bf9547f4d27ac013cd853b5f1412
anil pandiya <anilpandiya05@gmail.com>
2022-01-28 15:36:03 +0530

    Added Presentation pdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useContext } from 'react';
 
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const ThemeContext = createContext(themes.light);  //1. Creating a context
+ThemeContext.displayName = "Theme";
+
+const UserNameContext = createContext();
+UserNameContext.displayName = "User";
+
+//2. Providing context
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeContext.Provider value={themes.dark}> 
+        <Toolbar />
+      </ThemeContext.Provider>
+      <ThemeContext.Provider value={themes.light}> 
+        <Toolbar />
+      </ThemeContext.Provider>
     </div>
+  );
+}
+
+function Toolbar(props) {
+  return (
+    <div>
+      <UserNameContext.Provider value={'Jacob'}>
+        <ThemedButton />
+      </UserNameContext.Provider>
+    </div>
+  );
+}
+
+//3. consumption of context
+function ThemedButton() {
+  const theme = useContext(ThemeContext); 
+  const userName = useContext(UserNameContext);
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context! {userName}
+    </button>
   );
 }
 
